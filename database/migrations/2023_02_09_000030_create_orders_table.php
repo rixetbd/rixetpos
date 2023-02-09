e1868b26725603b3abe3c56e9ebaf253794e91d9
Muhammad Rabiul <rixetbd@gmail.com>
2023-02-09 20:07:58 +0600

    Initial commit

---
+++ database/migrations/2023_02_09_000030_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('customer_id');
+            $table->string('order_date');
+            $table->integer('total_amount');
+            $table->string('shipping_address');
+            $table->integer('payment_method');
+            $table->integer('payment_status');
+            $table->integer('order_status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
---
+++ database/migrations/2023_02_09_000042_create_purchases_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->id();
+            $table->integer('product_id');
+            $table->integer('supplier_id');
+            $table->integer('total_amount');
+            $table->integer('quantity');
+            $table->integer('payment_method');
+            $table->string('purchase_date');
+            $table->integer('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchases');
+    }
+};
